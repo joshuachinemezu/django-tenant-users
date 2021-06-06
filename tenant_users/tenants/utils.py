c94638c6fd5c49d18890883818ad3da35d2dc5aa
Joshua Chinemezu <joshuachinemezu@gmail.com>
2021-06-06 22:59:27 +0400

    Resolve issue with creating public tenant

--- tenant_users/tenants/utils.py
+++ tenant_users/tenants/utils.py
@@ -15,9 +15,10 @@ def get_current_tenant():
 def create_public_tenant(domain_url, owner_email, **owner_extra):
     UserModel = get_user_model()
     TenantModel = get_tenant_model()
+    TenantDataModel = get_tenant_domain_model()
     public_schema_name = get_public_schema_name()
 
-    if TenantModel.objects.filter(schema_name=public_schema_name).first():
+    if TenantDataModel.objects.filter(schema_name=public_schema_name).first():
         raise ExistsError("Public tenant already exists")
 
     # Create public tenant user. This user doesn't go through object manager
@@ -37,13 +38,13 @@ def create_public_tenant(domain_url, owner_email, **owner_extra):
             name='Public Tenant',
             owner=profile)
     else:
-        public_tenant = TenantModel.objects.create(schema_name=public_schema_name,
-                                                   name='Public Tenant',
+        public_tenant = TenantModel.objects.create(name='Public Tenant',
                                                    owner=profile)
 
         # Add one or more domains for the tenant
         domain = get_tenant_domain_model().objects.create(domain=domain_url,
                                                           tenant=public_tenant,
+                                                          schema_name=public_schema_name,
                                                           is_primary=True)
 
     # Add system user to public tenant (no permissions)
